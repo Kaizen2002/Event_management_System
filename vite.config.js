5f6f655b6fa87584412f41b3da924225034ff485
Kaizen <jyotirmay231@gmail.com>
2024-06-17 12:19:36 +0530

    Complete: Responsive Navbar

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    tailwindcss({
+      content: ["./*.html"],
+      theme: {
+        extend: {},
+      },
+    }),
+  ],
+});
